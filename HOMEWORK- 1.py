53fda846e50e8886ecc62c1b7c840be328edcf1f
Aradhya Negi <83121409+AradhyaNegi@users.noreply.github.com>
2021-04-28 15:41:04 +0530

    Add files via upload

---
+++ HOMEWORK- 1.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+CLASS HOMEWORK
+"""
+#TASK 1
+print("India is my country..lol")
+print("It is the seventh largest country in the world.")
+print("It has the 2nd largest road connections in the world.")
+print("It has 28 states and 9 union territories")
+print("I love my country and I am very proud to have it!")
+
+#TASK 3
+a = float(input(" Enter the First number : "))
+b = float(input(" Enter the Second number : "))
+d = a*b
+print(d)
+ 
+#TASK 4
+a = float(input(" Enter the First number : "))
+b = float(input(" Enter the Second number : "))
+d = a % b
+print(d)
+
+#TASK 6
+c = input(" Enter your City Name : ")
+print ("The " + c + "is an awesome place..")
+
+#TASK 7
+a = float(input(" Enter the First number : "))
+b = float(input(" Enter the Second number : "))
+d = a + b
+print(d)
+
+#TASK 8
+a = float(input(" Enter the First number : "))
+b = float(input(" Enter the Second number : "))
+c = float(input(" Enter the Third number : "))
+e = float(input(" Enter the Fourth number : "))
+f = float(input(" Enter the Fivth number : "))
+d = (a + b + c + e + f)/5
+print(d)
+
+#TASK 9
+a = input(" Enter the word which suits the pair  : Ups and ____ ")
+print("So the phrase formed is : Ups and " + a )
+
+#TASK 10
+q= Sheena
+v = input(" Enter your friends Name : ")
+if v == q:
+    print("She is indeed a good Friend")
+    
+else print("Nope don't know her..")
+
+
+
+
+
+
